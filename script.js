89a6d8b88c50b45829e64178fa788e808f0d018e
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-05-10 15:26:21 -0500

    added variables to the js script file

---
+++ script.js
@@ -0,0 +1,68 @@
+
+/*AS A coding bootcamp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers */
+var timer = "timer starts";
+var initials = "user initials";
+var score = "user scores";
+
+
+
+
+
+## Acceptance Criteria
+
+
+/* WHEN I click the start button */
+
+generateBtn.addEventListener("click", startGame);
+
+
+
+
+/*THEN a timer starts and I am presented with a question */
+
+
+
+/* WHEN I answer a question */
+
+var questions = ["Which animal never sleeps?", "Which big cat is the strongest?", "What is the fastest bird?", "What animal is extinct?", "Which creature has the largest brain in proportion to its body?"
+]
+var correctAnswers = [
+    "Bull Frogs", "Jaguar", "Peregrine Falcon", "Carolina Parakeet", "Elephants"
+]
+var wrongAnswers1 = [
+    "Toads", "Snakes", "Owl" 
+]
+var wrongAnswers2 = [
+    "Lion", "Tiger", "Leapard", 
+]
+var wrongAnswers3 = [
+    "Vulture", "The Golden Eagle", "The White-Throatex Needletial", 
+]
+var wrongAnswers4 = [
+    "White Tiger", "TRed Pandas", "White Lion", 
+]
+var wrongAnswers5 = [
+    "Dolphins", "Squirrels", "Sperm Whale", 
+]
+
+/*THEN I am presented with another question */
+
+
+/*WHEN I answer a question incorrectly*/
+ 
+
+
+/*THEN time is subtracted from the clock use timer code from color explosion activity*/
+
+
+/*WHEN all questions are answered or the timer reaches 0 THEN the game is over*/
+
+
+
+/*WHEN the game is over display high score*/
+
+
+
+/*THEN I can save my initials and score */
