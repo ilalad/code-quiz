29864d9022295c7c8583ba8aa701b57a88aa06fc
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-05-14 00:49:01 -0500

    revised code

--- script.js
+++ script.js
@@ -1,127 +1,120 @@
 var $time = document.querySelector("#time");
-var initials = "user initials";
-var score = "user scores";
-var userAnswer = "user input";
-var timeUp = "game over";
+var score = 0;
+var currentQuestion = 0;
+var totalQuestions = questions.length;
+
+var container = document.getElementById("quizContainer");
+var startButton = document.getElementById("startBtn");
+var questionElement = document.getElementById("question");
+var cho1 = document.getElementById("cho1");
+var cho2 = document.getElementById("cho2");
+var cho3 = document.getElementById("cho3");
+var cho4 = document.getElementById("cho4");
+
+var nextButton = document.getElementById("nextButton");
+var resultCount = document.getElementById("result");
 
 /* WHEN I click the start button */
-var startClick = document.querySelector("#startBtn");
 
-startClick.addEventListener("click", function () {
+startButton.addEventListener("click", function () {
   setTime();
-  startClick.style.display = "none";
-});
-var quiz = new Quiz(arrayOfQuestions);
-var choices = quiz.getQuestionIndex().choices;
-var i = 0;
-for (var i = 0; i < choices.length; i++) {
-  var element = document.getElementById("choice" + i);
-  element.innerHTML = choices[i];
-  guess("btn" + i, choices[i]);
-}
-showProgress();
+  startButton.style.display = "none";
 
-function setTime() {
-  var timeLeft = 60;
-  var timerInterval = setInterval(function () {
-    timeLeft--;
-    $time.textContent = "Time:" + timeLeft;
-    if (timeLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
-  }, 1000);
-}
+  function setTime() {
+    var timeLeft = 60;
+    var timerInterval = setInterval(function () {
+      timeLeft--;
+      $time.textContent = "Time:" + timeLeft;
+      if (timeLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+    }, 1000);
+  }
+});
 
 function sendMessage() {
   $time.textContent = "Game Over";
 }
-
-var arrayOfQuestions = [
-    new Question("Which animal never sleeps?",["Toads", "Snakes", "Owl"],"Bull Frogs"),
-
-    new Question("Which big cat is the strongest?",["Lion", "Tiger", "Leopard"],"Jaguar"),
-  
-    new Question("What is the fastest bird?",["Vulture","The Golden Eagle","The White-Throatex Needletial"],"Peregrine Falcon"),
-
-    new Question("What animal is extinct?",["White Tiger", "Red Pandas", "White Lion"],"Carolina Parakeet"),
-   
-    new Question("Which creature has the largest brain in proportion to its body?",["Dolphins", "Squirrels", "Sperm Whale", "Elephant"],"Elephant"),
-
-Quiz.prototype.getQuestionIndex = function () {
-  return this.arrayOfQuestions[this.questionIndex];
+function loadQuestions(questionIndex) {
+  var q = questions[questionIndex];
+  questionElement.textContent = questionIndex + 1 + "." + q.question;
+  cho1.textContent = q.choice1;
+  cho2.textContent = q.choice2;
+  cho3.textContent = q.choice3;
+  cho4.textContent = q.choice4;
 }
 
-Quiz.prototype.isEnded = function () {
-  return this.arrayOfQuestions.length === this.questionIndex;
-}
-Quiz.prototype.guess = function () {
-  if (this.arrayOfQuestions().correctAnswer(answer)) {
-    this.score++;
-  } else {
-    var element = document.getElementById("result");
-    element.innerHTML = "Wrong!";
+function loadNextQuestion() {
+  var selectedChoice = document.querySelector("input[type=radio]:checked");
+  if (!selectedChoice) {
+    alert("Please select your answer!");
+    return;
   }
-  if (this.arrayOfQuestions().correctAnswer(answer)) {
-    var element = document.getElementById("result");
-    element.innerHTML = "Correct!";
-  } else {
-    var element = document.getElementById("result");
-    element.innerHTML = "Wrong!";
+  var answer = selectedChoice.nodeValue;
+  if (question[currentQuestion].answer == answer) {
+    score += 10;
   }
-  this.questionIndex++;
-};
-function Question(text, choices, answer) {
-  this.text = text;
-  this.choices = choices;
-  this.answer = answer;
+  selectedChoice.checked = false;
+  currentQuestion++;
+  if (currentQuestion == totalQuestions - 10) {
+    nextButton.textContent = "Game Over";
+  }
+  if (currentQuestion == totalQuestions) {
+    container.style.display = "none";
+    resultCount.textContent = "Your Score: " + score;
+    return;
+  }
+  loadQuestions(currentQuestion);
 }
-Question.prototype.answer = function (choice) {
-  return choice === this.answer;
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  console.log("Started");
+  startButton.classList.add("hide");
+  questionContainerElement.classList.remove("hide");
+  loadNextQuestion();
 }
-function guess(id, guess) {
-  var button = document.getElementById(id);
-  button.onclick = function () {
-    quiz.guess(guess);
-    populate();
-  };
-}
-function showProgress() {
-    var currentQuestionNumber = quiz.questionIndex + 1;
-    var element = document.getElementById('progress');
-    element.innerHTML = "question " + currentQuestionNumber + " of " + quiz.questions.length;
-}
-function addInitial() {
-    var element = $("#initial").val();
-    console.log(element)
-    return element;
-}
-function showScores(initials) {
-    var initials = addInitial();
-    var gameOverHtml = "<h1>Result</h1>";
-    gameOverHtml += "<h2 id='score'> All Done! Your final score is: " + initials + " " + quiz.score + "</h2>";
-    var element = document.getElementById("quiz");
-    element.innerHTML = gameOverHtml;
-};
-populate();
-
-/*THEN a timer starts and I am presented with a question - PROPERTIES*/
-//refere to array crash course
-/* WHEN I answer a question - VALUES FOR QUESTION'S PROPERTIES*/
-
-// if answer is correct//
-//if (userAnswer === .correctAnswer) {
-//add to the number of correct answers
-//    score++;
-// }
-// else {
-//     alert ("wrong");
-// }
-
-/*THEN I am presented with another question */
-
-/*WHEN I answer a question incorrectly THEN time is subtracted from the clock use timer code from color explosion activity*/
-
-/*WHEN all questions are answered or the timer reaches 0 THEN the game is over*/
-
-// WHEN the game is over display high score THEN I can save my initials and score,//
+var questions = [
+  {
+    question: "Which animal never sleeps?",
+    choice1: "Toads",
+    choice2: "Snakes",
+    choice3: "Owl",
+    choice4: "Bull Frogs",
+    answer: "4",
+  },
+  {
+    question: "Which big cat is the strongest?",
+    choice1: "Jaguar",
+    choice2: "Lion",
+    choice3: "Tiger",
+    choice4: "Leopard",
+    answer: "1",
+  },
+  {
+    question: "What is the fastest bird?",
+    choice1: "Peregrine Falcon",
+    choice2: "The Golden Eagle",
+    choice3: "The White-Throatex Needletial",
+    choice4: "Vulture",
+    answer: "1",
+  },
+  {
+    question: "What animal is extinct?",
+    choice1: "White Tiger",
+    choice2: "White Lion",
+    choice3: "Carolina Parakeet",
+    choice4: "Red Pandas",
+    answer: "3",
+  },
+  {
+    question: "Which creature has the largest brain in proportion to its body?",
+    choice1: "Dolphins",
+    choice2: "Elephant",
+    choice3: "Lion",
+    choice4: "Sperm Whale",
+    answer: "4",
+  },
+];
